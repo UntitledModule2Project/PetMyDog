528d9eb5edc9b9ec5c77d593610777d6ec5b9dbf
Sarai Vasquez <svmarinez@hotmail.com>
2018-07-16 12:28:18 +0200

    Primeros cambios (main view and models)

--- models/User.js
+++ models/User.js
@@ -2,8 +2,11 @@ const mongoose = require('mongoose');
 const Schema   = mongoose.Schema;
 
 const userSchema = new Schema({
-  username: String,
-  password: String
+  username: {type: String, required: true},
+  password: {type: String, required: true},
+  email: String,
+  hasDog: {type: Boolean, default: "false"} ,
+  isActive: {type: Boolean, default: "true"}
 }, {
   timestamps: {
     createdAt: 'created_at',
